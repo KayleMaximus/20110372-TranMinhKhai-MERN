12ebcf12aa5e9e2dd01e0e10dfe069e8b876a3e5
Kayle Trần <kaylemaximus@gmail.com>
2023-10-05 00:05:58 +0700

    week05-middleware

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,7 +1,16 @@
-const siteRouter = require('./site');
-const userRouter = require('./user');
-function route(app) {
-    app.use('/users', userRouter);
-    app.use('/', siteRouter);
+const express = require('express');
+const router = express.Router();
+
+
+
+const homeRouter = require('./home');
+const postsRouter = require('./post');
+function route (app) {
+
+
+    // home page
+    app.use('/posts', postsRouter);
+    app.use('/', homeRouter);
 }
+
 module.exports = route;
